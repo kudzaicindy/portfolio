ad858dbbff901fec26e5d068c798360fac137435
kudzaicindy <165305935+kudzaicindy@users.noreply.github.com>
2024-11-12 11:51:04 +0200

    loll

---
+++ src/HashRouter.tsx
@@ -0,0 +1,12 @@
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+
+const HashRouter = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
+export default HashRouter; 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import HashRouter from './HashRouter';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HashRouter />
+  </React.StrictMode>
 );
